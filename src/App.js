8ac2dd7567cfa0b8a1a5450a7e3b9d1347528069
Shahabaj Bagwan <52992237+Shahabaj786786@users.noreply.github.com>
2025-12-15 19:33:00 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ETPOSDashboard from "./ETPOSDashboard";
+import AddUser from "./components/AddUser";
+import UserManagement from "./components/UserManagement";
 
 function App() {
-  return <ETPOSDashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ETPOSDashboard />}>
+          <Route index element={<Navigate to="/home" replace />} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="users/add" element={<AddUser />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
